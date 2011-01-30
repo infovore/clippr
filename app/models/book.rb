33b024db2f4ac11a7e07c76d1f87ee29244f47bb
Tom Armitage <tom@infovore.org>
2011-01-30 16:27:22 +0000

    Removing hybrid notes-and-clippings method; we just display clippings, and hook up notes.

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,4 @@ class Book < ActiveRecord::Base
   has_many :clippings
   has_many :notes
   
-  def clippings_and_notes
-    items = clippings + notes
-    items = items.sort_by {|i| i.loc}
-    items
-  end
 end
