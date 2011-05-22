2ce432b61b6fdd3dfd277f4be40cba17eb0a079f
Tom Armitage <tom@hideandseek.net>
2011-05-22 23:11:05 +0100

    Method to roughly find first matching instapaper reference.

--- app/models/instapaper_reference.rb
+++ app/models/instapaper_reference.rb
@@ -1,3 +1,29 @@
+require 'nokogiri'
+require 'open-uri'
 class InstapaperReference < ActiveRecord::Base
   belongs_to :clipping
+  
+  def self.find_or_create_from_clipping(clipping)
+    if clipping.instapaper_reference
+      clipping.instapaper_reference
+    else
+      ir = self.new
+      ir.clipping = clipping
+      
+      query = URI.escape(clipping.content)
+      doc = Nokogiri::HTML(open("http://www.google.com/search?q=#{query}"))
+
+      result = doc.css('h3.r a.l').first
+
+      if result
+        ir.url = result.attributes['href']
+        ir.title = result.content
+        if ir.save
+          ir
+        end
+      else
+        nil
+      end
+    end
+  end
 end
