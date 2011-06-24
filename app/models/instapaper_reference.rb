58767b94486e43dc21463d6a713fb7a7c89499da
Tom Armitage <tom@infovore.org>
2011-06-24 11:23:10 +0100

    Refactoring: no find_or_create method; instead, a method to find data for a clipping, and another method to prepopulate an unsaved object with that data.

--- app/models/instapaper_reference.rb
+++ app/models/instapaper_reference.rb
@@ -3,27 +3,30 @@ require 'open-uri'
 class InstapaperReference < ActiveRecord::Base
   belongs_to :clipping
   
-  def self.find_or_create_from_clipping(clipping)
-    if clipping.instapaper_reference
-      clipping.instapaper_reference
+  def self.find_data_for_clipping(clipping)
+    query = URI.escape(clipping.content)
+    doc = Nokogiri::HTML(open("http://www.google.com/search?q=#{query}"))
+
+    result = doc.css('h3.r a.l').first
+
+    if result
+      {:url => result.attributes['href'],
+       :title => result.content}
     else
-      ir = self.new
-      ir.clipping = clipping
-      
-      query = URI.escape(clipping.content)
-      doc = Nokogiri::HTML(open("http://www.google.com/search?q=#{query}"))
-
-      result = doc.css('h3.r a.l').first
-
-      if result
-        ir.url = result.attributes['href']
-        ir.title = result.content
-        if ir.save
-          ir
-        end
-      else
-        nil
-      end
+      nil
     end
   end
+  
+  def self.new_for_clipping(clipping)
+    data = find_data_for_clipping(clipping)
+
+    if data
+      self.new(:url => data[:url].value,
+               :title => data[:title],
+               :clipping_id => clipping.id)
+    else
+      self.new
+    end
+  end
+
 end
