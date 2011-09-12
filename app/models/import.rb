aa94e84f602dda48fe906261c0f9b439c4219c7f
Tom Armitage <tom@infovore.org>
2011-09-12 13:15:39 +0100

    Fixing import to work with the new text format found in Rule 34.

--- app/models/import.rb
+++ app/models/import.rb
@@ -36,13 +36,36 @@ class Import < ActiveRecord::Base
       author = Author.find_or_create_by_name(author)
       book = Book.find_or_create_by_title_and_author_id(title, author.id)
       
-      location,datetime = details.split("|")
-      
-      datetime = Time.parse(datetime.gsub("Added on ", "").strip)
-      
-      if location.match("Note")
-        location = location.gsub('- Note Loc. ', "").strip.to_i
-        related_clipping = Clipping.find_related_clipping(location)
+      is_note = false 
+      is_highlight = false
+
+      if details.scan("|").size == 2 # we have the new 2-item kind of matching
+        page,location,datetime_string = details.split("|")
+        if page.match("Note")
+          is_note = true
+          # NO IDEA WHAT TO DO HERE.
+        elsif page.match("Highlight")
+          is_highlight = true
+          locations = location.strip.gsub("Loc. ", "")
+          start_loc, end_loc = Clipping.location_string_to_array(locations)
+        end
+      else
+        location,datetime_string = details.split("|")
+        page = nil
+        if location.match("note")
+          is_note = true
+          location = location.gsub('- Note Loc. ', "").strip.to_i
+          related_clipping = Clipping.find_related_clipping(location)
+        elsif location.match("Highlight") # thus ignoring bookmarks.
+          locations = location.gsub('- Highlight Loc. ', "").strip
+          start_loc, end_loc = Clipping.location_string_to_array(locations)
+        end
+      end
+
+      datetime_string = datetime_string.gsub("Added on ", "").strip
+      datetime = Time.parse(datetime_string)
+
+      if is_note
         unless Note.first(:conditions => {:content => content, :clipped_at => datetime})
           Note.create(:content => content,
                       :clipped_at => datetime,
@@ -52,9 +75,7 @@ class Import < ActiveRecord::Base
                       :import => i,
                       :related_clipping => related_clipping)
         end
-      elsif location.match("Highlight") # thus ignoring bookmarks.
-        locations = location.gsub('- Highlight Loc. ', "").strip
-        start_loc, end_loc = Clipping.location_string_to_array(locations)
+      elsif is_highlight
         unless Clipping.first(:conditions => {:content => content, :clipped_at => datetime})
           Clipping.create(:content => content,
                           :clipped_at => datetime,
