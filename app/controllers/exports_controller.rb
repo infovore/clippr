9d0cf3e31c96b2d136295c63f3ea8bc3fb947f13
Tom Armitage <tom@infovore.org>
2011-09-12 13:06:12 +0100

    Latest work on exports

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -13,6 +13,7 @@ class ExportsController < ApplicationController
       @clippings = Clipping.all
     end
     # render clippings as xml
-    render :xml => @clippings.to_xml(:include => {:instapaper_references_controller => {}, :book => {:include => :author}})
+    #render :xml => @clippings.to_xml(:include => {:instapaper_references_controller => {}, :book => {:include => :author}})
+    render :xml => @clippings.to_xml_for_export
   end
 end
