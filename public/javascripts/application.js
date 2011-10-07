f2e20e5f042664329d7ca94f1b74931cd5f19180
Tom Armitage <tom@infovore.org>
2011-10-07 10:42:41 +0100

    First pass at adding dropdown for imports.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,9 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function() {
+  // from bootstrap-dropdown.js
+   $('#topbar').dropdown()
+
   $(".import-file-fields").hide();
   $(".file-import-dropdown").click(function() {
     $(".import-file-fields").toggle();
