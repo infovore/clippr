c72dd3ed8b4f2458ad164ef6d1a6b6172cdda1b6
Tom Armitage <tom@infovore.org>
2011-10-13 14:44:40 +0100

    Populate display name on create.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,12 +2,20 @@ class Book < ActiveRecord::Base
   belongs_to :author
   has_many :clippings, :dependent => :destroy
   has_many :notes, :dependent => :destroy
-  has_one :book_name, :dependent => :destroy
 
   acts_as_url :display_name, :url_attribute => :slug, :sync_url => true
 
+  after_create :populate_display_name
+
   def to_param
     slug
+  end
+
+  private
+
+  def populate_display_name
+    self.display_name = self.title
+    self.save
   end
 
 end
