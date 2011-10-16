7e9818339459780c0d0d21aae024d46e4420a999
Tom Armitage <tom@infovore.org>
2011-10-16 14:56:34 +0100

    Settings model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111011191317) do
+ActiveRecord::Schema.define(:version => 20111016130826) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
@@ -64,5 +64,16 @@ ActiveRecord::Schema.define(:version => 20111011191317) do
     t.datetime "updated_at"
     t.integer  "related_clipping_id"
   end
+
+  create_table "settings", :force => true do |t|
+    t.string   "var",                      :null => false
+    t.text     "value"
+    t.integer  "thing_id"
+    t.string   "thing_type", :limit => 30
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "settings", ["thing_type", "thing_id", "var"], :name => "index_settings_on_thing_type_and_thing_id_and_var", :unique => true
 
 end
