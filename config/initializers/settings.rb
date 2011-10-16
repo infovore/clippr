e8bb6e3d53c2a9ae24a921146e805da50aabdcf9
Tom Armitage <tom@infovore.org>
2011-10-16 20:00:05 +0100

    Adding defaults for HTML template export.

---
+++ config/initializers/settings.rb
@@ -0,0 +1,7 @@
+Settings.defaults[:single_clipping_html_template] = '<div class="clipping">
+$locations<blockquote>$quote</blockquote>
+$ifnote<p class="note">$note</p>$endifnote
+</div>
+'
+Settings.defaults[:book_title_html_template] = "<h3>$title by $author</h3>
+"
