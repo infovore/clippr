6909508d0bfd3989be2b03268983c2b1ede36044
Tom Armitage <tom@infovore.org>
2011-10-30 21:10:11 +0000

    Look at that: beautiful.

--- app/importers/kindle_importer.rb
+++ app/importers/kindle_importer.rb
@@ -2,54 +2,14 @@ class KindleImporter
   def self.process(input_text)
     chunks = input_text.gsub("\r", "").split("==========\n")
     output = chunks.map do |chunk|
-      lines = chunk.split("\n")
-      title_and_author = lines.shift
-      details = lines.shift
-      lines.shift
-      content = lines.join
+      title_and_author, details, content = split_up_chunk(chunk)
       
       title,author = dechunk_title_and_author(title_and_author)
-      
-      is_note = false 
-      is_highlight = false
-
-      if details.scan("|").size == 2 # we have the new 2-item kind of matching
-        page,location,datetime_string = details.split("|")
-        if page.match("Note")
-          is_note = true
-          # TODO: NO IDEA WHAT TO DO HERE.
-        elsif page.match("Highlight")
-          is_highlight = true
-          page = page.gsub(/\D/,"").to_i
-          locations = location.strip.gsub("Loc. ", "")
-          start_loc, end_loc = location_string_to_array(locations)
-        end
-      else
-        location,datetime_string = details.split("|")
-        page = nil
-        if location.match("Note")
-          is_note = true
-          location = location.gsub('- Note Loc. ', "").strip.to_i
-          start_loc,end_loc = location, location
-          #related_clipping = Clipping.find_related_clipping(location)
-        elsif location.match("Highlight") # thus ignoring bookmarks.
-          is_highlight = true
-          locations = location.gsub('- Highlight Loc. ', "").strip
-          start_loc, end_loc = location_string_to_array(locations)
-        end
-      end
-
-      datetime = datetime_from_string(datetime_string)
+      details_hash = parse_details(details)
 
       {:title => title, 
        :author => author,
-       :content => content,
-       :is_highlight => is_highlight,
-       :is_note => is_note,
-       :start_loc => start_loc,
-       :end_loc => end_loc,
-       :clipped_at => datetime,
-       :page => page}
+       :content => content}.merge(details_hash)
     end
     output
   end
@@ -67,6 +27,15 @@ class KindleImporter
       [start_loc.to_i, start_loc.to_i]
     end
   end
+
+  def self.split_up_chunk(chunk)
+    lines = chunk.split("\n")
+    title_and_author = lines.shift
+    details = lines.shift
+    lines.shift
+    content = lines.join
+    [title_and_author,details,content]
+  end
   
   def self.dechunk_title_and_author(title_and_author)
     # this needs to be sorted - add a "No Author" user on import.
@@ -83,6 +52,51 @@ class KindleImporter
   def self.datetime_from_string(datetime_string)
     datetime_string = datetime_string.gsub("Added on ", "").strip
     datetime = Time.parse(datetime_string)
+  end
+
+  def self.parse_details(details)
+    if details.scan("|").size == 2 # we have the new 2-item kind of matching
+      page,location,datetime_string = details.split("|")
+      if page.match("Note")
+        # TODO: NO IDEA WHAT TO DO HERE.
+        {:puzzled => true,
+         :is_note => true}
+      elsif page.match("Highlight")
+        page = page.gsub(/\D/,"").to_i
+        locations = location.strip.gsub("Loc. ", "")
+        start_loc, end_loc = location_string_to_array(locations)
+
+        {:page => page,
+         :start_loc => start_loc,
+         :end_loc => end_loc,
+         :is_highlight => true,
+         :clipped_at => datetime_from_string(datetime_string)}
+      else
+        {:oops => true}
+      end
+    else
+      location,datetime_string = details.split("|")
+      if location.match("Note")
+        is_note = true
+        location = location.gsub('- Note Loc. ', "").strip.to_i
+        start_loc,end_loc = location, location
+
+        {:start_loc => start_loc,
+         :end_loc => end_loc,
+         :is_note => true,
+         :clipped_at => datetime_from_string(datetime_string)}
+      elsif location.match("Highlight") # thus ignoring bookmarks.
+        locations = location.gsub('- Highlight Loc. ', "").strip
+        start_loc, end_loc = location_string_to_array(locations)
+
+        {:start_loc => start_loc,
+         :end_loc => end_loc,
+         :is_highlight => true,
+         :clipped_at => datetime_from_string(datetime_string)}
+      else
+        {:oops => true}
+      end
+    end
   end
 
 end
