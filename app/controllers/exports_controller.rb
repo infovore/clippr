67c1fea2119860d837440a48172a188c7de6a739
Tom Armitage <tom@infovore.org>
2011-11-14 13:55:38 +0000

    Fixing date-bounded export.

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -10,12 +10,13 @@ class ExportsController < ApplicationController
       # date selects
       start_date = [params[:start_date]["(1i)"], params[:start_date]["(2i)"], params[:start_date]["(3i)"]].join("-")
       end_date = [params[:end_date]["(1i)"], params[:end_date]["(2i)"], params[:end_date]["(3i)"]].join("-")
-      @clippings = Clipping.where("created_at >= ?", start_date).where("created_at < ?", end_date) 
+      start_datetime = DateTime.parse(start_date)
+      end_datetime = DateTime.parse(end_date)
+      @clippings = Clipping.where("clipped_at >= ?", start_datetime).where("clipped_at < ?", end_datetime) 
     else
       @clippings = Clipping.all
     end
     # render clippings as xml
-    #render :xml => @clippings.to_xml(:include => {:instapaper_references_controller => {}, :book => {:include => :author}})
     render :xml => @clippings.to_xml
   end
 
