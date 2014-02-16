244007cba7846291ca89906f2ab7b22bdd8431ed
Tom Armitage <tom@infovore.org>
2014-02-16 22:16:04 +0000

    This is how tempfiles work now.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,10 @@ class AuthorsController < ApplicationController
 
   def index
     @authors = Author.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @authors }
+    end
   end
   
   def show
--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -4,7 +4,8 @@ class ImportsController < ApplicationController
     if params[:import] && params[:import][:import_file]
       
       begin
-        new_items_count = Import.perform_import_from_raw_text(params[:import][:import_file].read)
+        raw_text = File.read(params[:import][:import_file].tempfile.path, :encoding => 'UTF-8')
+        new_items_count = Import.perform_import_from_raw_text(raw_text)
 
         flash[:success] = "#{help.pluralize(new_items_count, "new item")} imported."
         redirect_to "/"
